09cfe16aa6aad80578b8e4157e32c5a26e020e75
Michael <31572980+mjohnson92@users.noreply.github.com>
2018-02-06 08:57:47 -0700

    Pushing page
    hello world!

---
+++ hw-2/script.js
@@ -0,0 +1,2 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>I'm excited to learn Javascript and make my web pages more interactive! Although I do fear it's going to get pretty difficult! :) </p>");
