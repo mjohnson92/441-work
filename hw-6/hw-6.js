2251157bb77fb148beb8270c636ccc26ba8b9959
Michael <31572980+mjohnson92@users.noreply.github.com>
2018-03-13 19:34:24 -0600

    x
    x

--- hw-6/hw-6.js
+++ hw-6/hw-6.js
@@ -27,8 +27,8 @@ To begin, you should select the body element from the DOM, then remove its exist
 
 */
 
-var albumImage = new Image (750, 750);
-albumImage.src = "../HW-6/images/humanlines.jpg"
+var myImage = new Image (750, 750);
+myImage.src = "../HW-6/images/humanlines.png"
 
 //body
 let body = document.body;
@@ -42,27 +42,27 @@ let element3 = document.createElement("p");
 let element4 = document.createElement("p");
 let element5 = document.createElement("a");
 
-element1.innerText = "Javascript Homework";
+element1.innerText = "Javascript Homework - Michael Johnson";
 element2.innerText = "Astrid Sonne - Human Lines";
 element3.innerText = "I've been looking for new music lately and have really enjoyed this album. She is from Copenhagen and makes experimental ambient / classical."
 element4.innerText = "My favorite track off of the album is called Alto. It's shows more of her classical skill and its gorgeous."
 element5.innerText = "Read & learn more about Astrid Sonne";
 
 element.setAttribute("style", "text-align:center");
-element1.setAttribute("style", "padding:0.5em; margin:0; font-style:italic; text-align:center; color:#000000");
-element2.setAttribute("style", "padding:0em; margin:0; text-align:center; color:#6b5f44");
-element3.setAttribute("style", "padding:0em; margin:0; font-style:italic; text-align:center; color:#000000");
-element4.setAttribute("style", "padding:0em; margin:0; font-style:italic; text-align:center; color:#000000");
+element1.setAttribute("style", "padding:0.5em; margin:0; text-align:center; color:#000000");
+element2.setAttribute("style", "padding:2em; margin:0; font-style:bold; text-align:center; color:#6b5f44");
+element3.setAttribute("style", "padding:.5em; margin:0; font-style:italic; text-align:center; color:#000000");
+element4.setAttribute("style", "padding:.5em; margin:0; font-style:italic; text-align:center; color:#000000");
 element5.setAttribute("href", "http://www.factmag.com/2018/02/28/astrid-sonne-fact-rated/");
-element5.setAttribute("style", "padding:0em; margin:0; font-style:bold; text-align:right; color:#6b5f44");
+element5.setAttribute("style", "padding:2em; margin:0; font-style:italic; text-align:right; color:#6b5f44");
 
-myImage.setAttribute("style", "padding:0em; margin:100px");
+myImage.setAttribute("style", "padding:0em;  margin:220px");
 
 element.appendChild(element1);
 element.appendChild(element2);
 element.appendChild(element3);
 element.appendChild(element4);
-element.appendChild(albumImage);
+element.appendChild(myImage);
 element.appendChild(element5);
 
 body.appendChild(element);
